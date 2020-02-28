4ac3a96cb089868ac0dcbd3f5120b74f9237048d
petepham <ppham.xc@gmail.com>
2020-02-27 17:05:35 -0800

    prepared code for introduction of deer data

--- traffic.frequency.r
+++ traffic.frequency.r
@@ -50,19 +50,19 @@ p.mean
 p.day
 
 #Overlap
-d.tot <- data.frame(read_excel("trafficdata.xlsx", sheet = "tot"))
+d.deer <- d.e[10]
 
-m.tot <- d.tot %>%
+m.deer <- d.deer %>%
   gather(key="text", value="value") %>%
   mutate(text = gsub("\\.", " ",text)) %>%
   mutate(value = round(as.numeric(value),0))
 
-df.tot <- data.frame(hour = rep(0:23, each = 1), rbind(d.tot))
+df.tot <- data.frame(hour = rep(0:23, each = 1), rbind(m.deer))
 
-p.tot <- ggplot(df.tot, aes(fill=as.factor(hour), y=tot, x=as.factor(hour))) + 
+p.tot <- ggplot(df.tot, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
   geom_bar(position="stack", stat="identity") +
   scale_fill_viridis(discrete = T, option = "E") +
-  ggtitle("Mean Weekday Frequency of Eastbound and Westbound Traffic") +
+  ggtitle("Mean Weekly Frequency Against Deer") +
   theme_ipsum() +
   theme(legend.position="none") +
   xlab("Hour") +
