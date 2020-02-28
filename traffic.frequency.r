dc74f573fe2b31c30a6154039e0fb2a90a6f60c7
petepham <ppham.xc@gmail.com>
2020-02-27 17:01:23 -0800

    aggregated data for east & westbound traffic

--- traffic.frequency.r
+++ traffic.frequency.r
@@ -8,94 +8,64 @@ library(tidyverse)
 
 
 #Import Dataset
-d.eb <- data.frame(read_excel("trafficdata.xlsx", sheet = "eb"))
-d.wb <- data.frame(read_excel("trafficdata.xlsx", sheet = "wb"))
+d.e <- data.frame(read_excel("trafficdata.xlsx"))
 
-d.mean.e <- d.eb[c(9:11)]     #For mean aggregates
-d.day.e <- d.eb[c(2:8)]       #For each individual day
+d.mean <- d.e[c(8:10)]     #For mean aggregates
+d.day <- d.e[c(1:7)]       #For each individual day
 
-d.mean.w <- d.wb[c(7)]     #For mean aggregates
-d.day.w <- d.wb[c(2:6)]    #For each individual day
-
-dm.m.e <- d.mean.e %>%
+dm.m <- d.mean %>%
   gather(key="text", value="value") %>%
   mutate(text = gsub("\\.", " ",text)) %>%
   mutate(value = round(as.numeric(value),0))
 
-dm.d.e <- d.day.e %>%
+dm.d <- d.day %>%
   gather(key="text", value="value") %>%
   mutate(text = gsub("\\.", " ",text)) %>%
   mutate(value = round(as.numeric(value),0))
 
-dm.m.w <- d.mean.w %>%
-  gather(key="text", value="value") %>%
-  mutate(text = gsub("\\.", " ",text)) %>%
-  mutate(value = round(as.numeric(value),0))
-
-dm.d.w <- d.day.w %>%
-  gather(key="text", value="value") %>%
-  mutate(text = gsub("\\.", " ",text)) %>%
-  mutate(value = round(as.numeric(value),0))
-
-d.m.e <- data.frame(hour = rep(0:23, each = 1), rbind(dm.m.e))
-d.d.e <- data.frame(hour = rep(0:23, each = 1), rbind(dm.d.e))
-d.m.w <- data.frame(hour = rep(0:23, each = 1), rbind(dm.m.w))
-d.d.w <- data.frame(hour = rep(0:23, each = 1), rbind(dm.d.w))
+d.m <- data.frame(hour = rep(0:23, each = 1), rbind(dm.m))
+d.d <- data.frame(hour = rep(0:23, each = 1), rbind(dm.d))
 
 
 #Generate Plots
-p.m.e <- ggplot(d.m.e, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
+p.mean <- ggplot(d.m, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
   geom_bar(position="dodge", stat="identity") +
   scale_fill_viridis(discrete = T, option = "E") +
-  ggtitle("Aggregate Mean Frequency For Eastbound Traffic") +
+  ggtitle("Aggregate Mean Frequency for Boeckman Traffic") +
   facet_wrap(~text) +
   theme_ipsum() +
   theme(legend.position="none") +
   xlab("")
 
-p.d.e <- ggplot(d.d.e, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
+p.day <- ggplot(d.d, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
   geom_bar(position="dodge", stat="identity") +
   scale_fill_viridis(discrete = T, option = "E") +
-  ggtitle("Daily Frequency for Eastbound Traffic") +
+  ggtitle("Daily Frequency for Boeckman Traffic") +
   facet_wrap(~text) +
   theme_ipsum() +
   theme(legend.position="none") +
   xlab("")
 
-p.m.w <- ggplot(d.m.w, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
-  geom_bar(position="dodge", stat="identity") +
-  scale_fill_viridis(discrete = T, option = "E") +
-  ggtitle("Aggregate Mean Frequency for Westbound Traffic") +
-  facet_wrap(~text) +
-  theme_ipsum() +
-  theme(legend.position="none") +
-  xlab("")
-
-p.d.w <- ggplot(d.d.w, aes(fill=as.factor(hour), y=value, x=as.factor(hour))) + 
-  geom_bar(position="dodge", stat="identity") +
-  scale_fill_viridis(discrete = T, option = "E") +
-  ggtitle("Daily Frequency for Westbound") +
-  facet_wrap(~text) +
-  theme_ipsum() +
-  theme(legend.position="none") +
-  xlab("")
-
-
-p.m.e
-p.d.e
-p.m.w
-p.d.w
-
+p.mean
+p.day
 
 #Overlap
 d.tot <- data.frame(read_excel("trafficdata.xlsx", sheet = "tot"))
 
-p.tot <- ggplot(d.tot, aes(fill=as.factor(h), y=wb, x=as.factor(h))) + 
+m.tot <- d.tot %>%
+  gather(key="text", value="value") %>%
+  mutate(text = gsub("\\.", " ",text)) %>%
+  mutate(value = round(as.numeric(value),0))
+
+df.tot <- data.frame(hour = rep(0:23, each = 1), rbind(d.tot))
+
+p.tot <- ggplot(df.tot, aes(fill=as.factor(hour), y=tot, x=as.factor(hour))) + 
   geom_bar(position="stack", stat="identity") +
   scale_fill_viridis(discrete = T, option = "E") +
   ggtitle("Mean Weekday Frequency of Eastbound and Westbound Traffic") +
   theme_ipsum() +
   theme(legend.position="none") +
-  xlab("")
+  xlab("Hour") +
+  ylab("Count")
 
 p.tot
